4038d09ad21612a71fde2e16e10e22d3b8702592
“laurendoesntlikeusernames” <Lauren.langford.mail@gmail.com>
2022-08-01 17:02:33 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import {StackDivider, VStack} from '@chakra-ui/react'
+import {Container, VStack} from '@chakra-ui/react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
 
 export default function Index() {
   return (
-    <VStack spacing={4} align='stretch'>
-      <Header />
-      <Hero />
+    <VStack spacing={4} align='stretch' width='100vh'>
+      <Container centerContent bg='red.400'>
+        <Header />
+      </Container>
+      <Container centerContent bg='blue.400'>
+        <Hero />
+      </Container>
     </VStack>     
   )
 }
