e66649ba293bb53ec22ca1ef1deae8693dca0dd4
“laurendoesntlikeusernames” <Lauren.langford.mail@gmail.com>
2022-08-19 11:41:42 +0800

    chakra form option 1

--- components/ClientForm.jsx
+++ components/ClientForm.jsx
@@ -2,15 +2,21 @@ import { FormEvent, ChangeEvent, useState } from 'react';
 import {
   Stack,
   FormControl,
+  FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   Button,
   useColorModeValue,
   Heading,
   Text,
+  Box,
+  HStack,
   Container,
   Flex,
+  Link
 } from '@chakra-ui/react';
-import { CheckIcon } from '@chakra-ui/icons';
+import { CheckIcon, ViewOffIcon, ViewIcon } from '@chakra-ui/icons';
 import { useFormik } from "formik";
 
 const ClientForm = () => {
@@ -21,20 +27,86 @@ const ClientForm = () => {
         onSubmit: (values) => {
           alert(JSON.stringify(values, null, 2))
         },
-  })  
+  }) 
+  const [showPassword, setShowPassword] = useState(false); 
   return (
+    <>
     <form onSubmit={formik.handleSubmit}>
-    <label htmlFor='email'>Email Address</label>
-    <Input
+      <label htmlFor='email'>Email Address</label>
+      <Input
         id='email'
         name='email'
         type='email'
         onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+        value={formik.values.email} />
 
       <Button type='submit'>Submit</Button>
-  </form>
+    </form><Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Sign up
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool features ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? 'text' : 'password'} />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={'center'}>
+                Already a user?<Link color={'blue.400'}>Login</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack></>
     
   )
 }
--- pages/Started.jsx
+++ pages/Started.jsx
@@ -9,18 +9,19 @@ import {
   Text,
   Box,
   Button,
-
 } from '@chakra-ui/react';
+import Header from '../components/Header'
+
 
 const Started = () => {
   return (
-    <Container maxW={'5xl'}>
-        <Stack
+    <><Header /><Container maxW={'5xl'}>
+      <Stack
         textAlign={'center'}
         align={'center'}
         spacing={{ base: 8, md: 10 }}
         py={{ base: 20, md: 28 }}>
-          <Heading
+        <Heading
           fontWeight={600}
           fontSize={{ base: '6xl', sm: '6xl', md: '6xl' }}
           lineHeight={'110%'}>
@@ -28,26 +29,26 @@ const Started = () => {
           <Text as={'span'} color={'orange.400'}>
             from Perth
           </Text>
-          </Heading>
-          <Flex minWidth='max-content' alignItems='center'>
-            <VStack spacing='24px'>
+        </Heading>
+        <Flex minWidth='max-content' alignItems='center'>
+          <VStack spacing='24px'>
 
-              <ClientForm />
+            <ClientForm />
 
-              <Box w='500px' h='40px' bg='yellow.200'>
-                
-              </Box>
-              <Box w='500px' h='40px' bg='tomato'>
-                
-              </Box>
-              <Box w='500px' h='40px' bg='pink.100'>
-          
-              </Box>
-             
-            </VStack>
-          </Flex> 
-        </Stack>
-    </Container>
+            <Box w='500px' h='40px' bg='yellow.200'>
+
+            </Box>
+            <Box w='500px' h='40px' bg='tomato'>
+
+            </Box>
+            <Box w='500px' h='40px' bg='pink.100'>
+
+            </Box>
+
+          </VStack>
+        </Flex>
+      </Stack>
+    </Container></>
   )
 }
 
