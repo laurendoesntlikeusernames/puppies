1bc104e0dca70013a74019f0dbcd51872ae015f4
“laurendoesntlikeusernames” <Lauren.langford.mail@gmail.com>
2022-08-22 10:40:33 +0800

    link up formik to the form

--- components/ClientForm.jsx
+++ components/ClientForm.jsx
@@ -16,29 +16,30 @@ import {
   Flex,
   Link
 } from '@chakra-ui/react';
-import { CheckIcon, ViewOffIcon, ViewIcon } from '@chakra-ui/icons';
 import { useFormik } from "formik";
 
 const ClientForm = () => {
     const formik = useFormik({
         initialValues: {
+          firstName: '',
+          lastName: '',
+          petName: '',
           email: '',
         },
         onSubmit: (values) => {
           alert(JSON.stringify(values, null, 2))
         },
   }) 
-  const [showPassword, setShowPassword] = useState(false); 
   return (
     <form onSubmit={formik.handleSubmit}>
 
-    <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+    <Stack spacing={8} mx={'auto'} maxW={'3xl'} py={12} px={6}>
         <Stack align={'center'}>
           <Heading fontSize={'4xl'} textAlign={'center'}>
-            Client Request Form
+            How can we help you today?
           </Heading>
           <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool features ✌️
+            Initial client request form? 🐕
           </Text>
         </Stack>
         <Box
@@ -48,18 +49,22 @@ const ClientForm = () => {
           p={8}>
           <Stack spacing={4}>
             <HStack>
-              <Box>
-                <FormControl id="firstName" isRequired>
-                  <FormLabel>First Name</FormLabel>
+        
+                <FormControl id="firstName" isRequired
+                  onChange={formik.handleChange}
+                  value={formik.values.firstName}>
+                  <FormLabel>Owner First Name</FormLabel>
                   <Input type="text" />
                 </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName">
-                  <FormLabel>Last Name</FormLabel>
+      
+                <FormControl 
+                id="lastName" isRequired
+                onChange={formik.handleChange}
+                value={formik.values.lastName}>
+                  <FormLabel>Owner Last Name</FormLabel>
                   <Input type="text" />
                 </FormControl>
-              </Box>
+             
             </HStack>
 
             <FormControl 
@@ -74,39 +79,36 @@ const ClientForm = () => {
               <Input type="email" />
             </FormControl>
 
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'}
-                    onClick={() => setShowPassword((showPassword) => !showPassword)}>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
+           
+              <Box>
+                <FormControl id="petName" isRequired
+                  onChange={formik.handleChange}
+                  value={formik.values.lastName}>
+                  <FormLabel>Pet's Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+   
+
             <Stack spacing={10} pt={2}>
               
+          
+
               <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-                type='submit'
-                >
-                Submit
+                  loadingText="Submitting"
+                  size="md"
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                  type='submit'
+                  >
+                  Submit
               </Button>
+
+   
             
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user?<Link color={'blue.400'}>Login</Link>
-              </Text>
             </Stack>
           </Stack>
         </Box>
--- zscrap.jsx
+++ zscrap.jsx
@@ -1,3 +1,4 @@
+<>
 <form onSubmit={formik.handleSubmit}>
 <label htmlFor='email'>Email Address</label>
 <Input
@@ -9,3 +10,26 @@
 
 <Button type='submit'>Submit</Button>
 </form>
+const [showPassword, setShowPassword] = useState(false); 
+
+<FormControl id="password" isRequired>
+<FormLabel>Password</FormLabel>
+<InputGroup>
+  <Input type={showPassword ? 'text' : 'password'} />
+  <InputRightElement h={'full'}>
+    <Button
+      variant={'ghost'}
+      onClick={() => setShowPassword((showPassword) => !showPassword)}>
+      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+    </Button>
+  </InputRightElement>
+</InputGroup>
+</FormControl>
+
+
+<Stack pt={6}>
+              <Text align={'center'}>
+                Already a user?<Link color={'blue.400'}>Login</Link>
+              </Text>
+            </Stack>
+</>
