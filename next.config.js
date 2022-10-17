e0e17a00bf5407f84908dbe5a3ea33b05463c068
“laurendoesntlikeusernames” <Lauren.langford.mail@gmail.com>
2022-10-18 07:50:38 +0800

    tidy up

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+    return config;
+  }
 }
-
-module.exports = nextConfig
--- pages/sign-up.jsx
+++ pages/sign-up.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Subscribe from '../components/SubscribeForm'
 
 const signup = () => {
   return (
-    <Subscribe />
-  )
+<></>
+    )
 }
 
 export default signup
