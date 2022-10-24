81b43faea632cc60b38f4049c2f4822a853a1deb
“laurendoesntlikeusernames” <Lauren.langford.mail@gmail.com>
2022-10-24 10:38:07 +0800

    fixed nav

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         <Heading noOfLines={2}
         fontWeight={600}
         fontSize={{ base: '6xl', sm: '6xl', md: '7xl' }}
-        marginTop={{base: '50'}}
+        marginTop={{base: '60'}}
         lineHeight={'130%'}
         textAlign="center">
         The puppy sitters from <br />
